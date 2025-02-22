0b2ce3d07f43df7f5e1cf3e24ef6c8bb9d82f9ad
Veronika Rouba <Roubaveronika@gmail.com>
2025-02-22 12:46:53 +0100

    lesson3.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(data1,data2,data3) {
+    return Math.max(data1,data2,data3);
+}
+console.log(findLargest(10,20,30)); // Output: 30
+console.log(findLargest(5,10,15)); // Output: 15
